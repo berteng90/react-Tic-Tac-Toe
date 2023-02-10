14124591abe054d985f78f1d04072723685bc967
unknown <berteng90@gmail.com>
2023-02-10 10:36:45 +0800

    Update Game logic

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -3,24 +3,56 @@ import Square from "./Square";
 
 export default function GameBoard() {
   const [square, setSquare] = useState(Array(9).fill(null));
-
+  const [playerSquare] = useState(Array().fill(null));
+  const [computerSquare] = useState(Array().fill(null));
   function handleClick(i) {
     const nextSquare = square.slice();
     nextSquare[i] = "X";
+    playerSquare.push(i);
     handleAI(nextSquare);
+    checkForWinner(playerSquare, "Player");
   }
 
   function handleAI(nextSquare) {
-    console.log(nextSquare);
     const random = Math.floor(Math.random() * 9);
     if (nextSquare[random] === null) {
       nextSquare[random] = "O";
+      computerSquare.push(random);
+      checkForWinner(computerSquare, "Computer");
       setSquare(nextSquare);
     } else {
       handleAI(nextSquare);
     }
   }
 
+  function checkForWinner(moves, player) {
+    // const winningCombinations = [
+    //   [0, 1, 2],
+    //   [3, 4, 5],
+    //   [6, 7, 8],
+    //   [0, 3, 6],
+    //   [1, 4, 7],
+    //   [2, 5, 8],
+    //   [0, 4, 8],
+    //   [2, 4, 6],
+    // ];
+    // for (let i = 0; i < winningCombinations.length; i++) {
+    //   for (let j = 0; j < winningCombinations[i].length; ) {
+    //     if (winningCombinations[i][j] === moves[j]) {
+    //       j++;
+    //       if (winningCombinations[i][j] === moves[j]) {
+    //         j++;
+    //         if (winningCombinations[i][j] === moves[j]) {
+    //           alert(`${player} Wins!!`);
+    //         }
+    //         j = 0;
+    //       }
+    //       j = 0;
+    //     }
+    //     j = 0;
+    //   }
+    // }
+  }
   return (
     <div id="gameboard">
       <div className="board-row">
